3f0770cd24719574f88a213f1b0c8f9847e412cc
Walteriano <walter.8128@gmail.com>
2024-09-30 00:31:49 -0300

    Iniciando_desafio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbarr from './Navbar';
+import Home from './Home';
+import Footer from './Footer';
+//import Register from './Register';
+//import Login from './Login';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbarr />
+        <Home /> 
+        {/*<Register />*/}
+        {/*<Login />*/}
+        <Footer />
+      </div>  
+    </>
+  )
+}
+
+export default App
